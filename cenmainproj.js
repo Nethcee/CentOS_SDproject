f28c3b6a3e907d30c6c98619cfb437940451e617
Nethcee <110341794+Nethcee@users.noreply.github.com>
2025-03-15 12:49:55 +0800

    Add files via upload

---
+++ cenmainproj.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    document.querySelector('.splash').style.display = 'none';
+
+    document.querySelector('.loginHeader').style.display = 'block';
+    document.querySelector('.loginBody').style.display = 'block';
+}, 2000);

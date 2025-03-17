56d7790affcd3c30fcfe5e7ab45dc73201d16302
Nethcee <110341794+Nethcee@users.noreply.github.com>
2025-03-17 13:54:49 +0800

    Delete cenmainproj.js

--- cenmainproj.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-    document.querySelector('.splash').style.display = 'none';
-
-    document.querySelector('.loginHeader').style.display = 'block';
-    document.querySelector('.loginBody').style.display = 'block';
-}, 2000);
